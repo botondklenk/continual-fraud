d17a3bc83b04840ca94d5409122e8e90dffbbe43
Bence Benyak <bence.benyak@aimotive.com>
2024-05-18 12:56:32 +0200

    Fix loader

--- src/data/loader.py
+++ src/data/loader.py
@@ -11,25 +11,26 @@ class DataLoader:
         X = df.drop('is_fraud', axis=1)
         y = df['is_fraud']
         return X, y
-    
+
     def load_state_by_name(self, state):
         df = pd.read_csv(self.path)
         df_state = df[df['state'] == state]
         X = df_state.drop('is_fraud', axis=1)
         y = df_state['is_fraud']
         return X, y
-    
+
     ## Get state by id, sorted by the number of transactions
     def load_state_by_id(self, state_id):
+        train_df = pd.read_csv("../../data/fraudTrain.csv")
         df = pd.read_csv(self.path)
-        state_counts = df['state'].value_counts().sort_values(ascending=False)
+        state_counts = train_df['state'].value_counts().sort_values(ascending=False)
         state_index = state_counts.index.to_list()
         state = state_index[state_id]
         df_state = df[df['state'] == state]
         X = df_state.drop('is_fraud', axis=1)
         y = df_state['is_fraud']
         return X, y
-    
+
     def get_state_list_size(self):
         df = pd.read_csv(self.path)
         state_counts = df['state'].value_counts().sort_values(ascending=False)
